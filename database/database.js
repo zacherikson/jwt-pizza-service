df51776d053da2cbe1999c6c46944a59cd408aab
Lee <leesjensen@gmail.com>
2024-04-27 21:28:29 -0600

    add(menu) to database

--- database/database.js
+++ database/database.js
@@ -6,11 +6,16 @@ class DB {
     this.initializeDatabase();
   }
 
+  async getMenu() {
+    const rows = await this.query(`SELECT * FROM menu`);
+    return rows;
+  }
+
   async getOrders(page = 1) {
     const offset = this.getOffset(page, config.db.listPerPage);
     const rows = await this.query(
       `SELECT id, franchise, store, data 
-      FROM orders LIMIT ${offset},${config.db.listPerPage}`
+      FROM dinerOrder LIMIT ${offset},${config.db.listPerPage}`
     );
     const data = this.emptyOrRows(rows);
     const meta = { page };
@@ -22,7 +27,7 @@ class DB {
   }
 
   async addOrder(order) {
-    const sql = `INSERT INTO orders (franchise, store, data) VALUES (?, ?, ?)`;
+    const sql = `INSERT INTO dinerOrder (franchise, store, data) VALUES (?, ?, ?)`;
     const result = await this.query(sql, [order.franchise, order.store, order.data]);
     order.id = result.insertId;
     return order;
@@ -46,6 +51,25 @@ class DB {
     return results;
   }
 
+  async addDefaultMenu(connection) {
+    const defaultMenu = [
+      { title: 'Veggie', description: 'A garden of delight', image: 'pizza1.png', price: 0.0038 },
+      { title: 'Pepperoni', description: 'Spicy treat', image: 'pizza2.png', price: 0.0042 },
+      { title: 'Margarita', description: 'Essential classic', image: 'pizza3.png', price: 0.0014 },
+      { title: 'Crusty', description: 'A dry mouthed favorite', image: 'pizza4.png', price: 0.0024 },
+      { title: 'Flat', description: 'Something special', image: 'pizza5.png', price: 0.0028 },
+      { title: 'Chared Leopard', description: 'For those with a darker side', image: 'pizza6.png', price: 0.0099 },
+    ];
+
+    const [rows] = await connection.execute(`SELECT COUNT(*) as count FROM menu`);
+    const rowCount = rows[0].count;
+    if (rowCount === 0) {
+      for (const menuItem of defaultMenu) {
+        await connection.execute(`INSERT INTO menu (title, description, price, image) VALUES (?, ?, ?, ?)`, [menuItem.title, menuItem.description, menuItem.price, menuItem.image]);
+      }
+    }
+  }
+
   async initializeDatabase() {
     try {
       const connection = await mysql.createConnection({
@@ -60,13 +84,25 @@ class DB {
       await connection.query(`USE ${config.db.connection.database}`);
 
       await connection.query(`
-            CREATE TABLE IF NOT EXISTS orders (
+            CREATE TABLE IF NOT EXISTS dinnerOrder (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 franchise VARCHAR(255) NOT NULL,
                 store VARCHAR(255) NOT NULL,
                 data TEXT NOT NULL
             )
-        `);
+      `);
+
+      await connection.query(`
+            CREATE TABLE IF NOT EXISTS menu (
+              id INT AUTO_INCREMENT PRIMARY KEY,
+              title VARCHAR(255) NOT NULL,
+              image VARCHAR(1024) NOT NULL,
+              price DECIMAL(10, 2) NOT NULL,
+              description TEXT NOT NULL
+            )
+      `);
+
+      await this.addDefaultMenu(connection);
 
       console.log('Database initialized successfully');
     } catch (err) {
--- routes/pizzaRouter.js
+++ routes/pizzaRouter.js
@@ -4,20 +4,11 @@ import authRouter from './authRouter.js';
 
 const pizzaRouter = express.Router();
 
-pizzaRouter.get('/menu', (req, res) => {
-  res.send({
-    menu: [
-      { title: 'Veggie', description: 'A garden of delight', image: 'pizza1.png', price: 0.00038 },
-      { title: 'Pepperoni', description: 'Spicy treat', image: 'pizza2.png', price: 0.00042 },
-      { title: 'Margarita', description: 'Essential classic', image: 'pizza3.png', price: 0.00014 },
-      { title: 'Crusty', description: 'A dry mouthed favorite', image: 'pizza4.png', price: 0.00024 },
-      { title: 'Flat', description: 'Something special', image: 'pizza5.png', price: 0.00028 },
-      { title: 'Chared Leopard', description: 'For those with a darker side', image: 'pizza6.png', price: 0.00099 },
-    ],
-  });
+pizzaRouter.get('/menu', async (req, res) => {
+  res.send(await db.getMenu());
 });
 
-pizzaRouter.get('/', authRouter.authenticateToken, async function (req, res) {
+pizzaRouter.get('/', authRouter.authenticateToken, async (req, res) => {
   res.json(await db.getOrders(req.query.page));
 });
 
