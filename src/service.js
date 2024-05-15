403657458c95f30a9c057575c8d579a1d322135e
Lee <leesjensen@gmail.com>
2024-05-14 20:07:43 -0600

    fix(service) error handling

--- src/service.js
+++ src/service.js
@@ -39,6 +39,7 @@ app.use('*', (req, res) => {
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(err.statusCode ?? 500).json({ message: err.message, stack: err.stack });
+  next();
 });
 
 module.exports = app;
