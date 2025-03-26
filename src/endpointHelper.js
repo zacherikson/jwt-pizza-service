41d50fa1268a770e17da2d9747ab9cf7d80516cf
zacherikson <zacharyerikson@gmail.com>
2025-03-25 19:26:51 -0600

    work

--- src/endpointHelper.js
+++ src/endpointHelper.js
@@ -1,5 +1,5 @@
 const Logger = require("pizza-logger");
-const config = require("config.js");
+const config = require("./config.js");
 const logger = new Logger(config);
 
 class StatusCodeError extends Error {
