d4a633e9422d06d4c3fe7a2c405a9330e20dd2b0
zacherikson <zacharyerikson@gmail.com>
2025-04-01 15:42:18 -0600

    stupid crap

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,5 @@ export default [
   { languageOptions: { globals: globals.node } },
   { languageOptions: { globals: globals.jest } },
   pluginJs.configs.recommended,
+  { ignores: ["loadTests/loginAndOrder.js"] },
 ];
