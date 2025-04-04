7b4eeb5dba40bd03fb2375c923f850f7a782e421
zacherikson <zacharyerikson@gmail.com>
2025-04-04 15:29:42 -0600

    Chaos test

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -157,4 +157,24 @@ orderRouter.post(
   })
 );
 
+let enableChaos = false;
+orderRouter.put(
+  "/chaos/:state",
+  authRouter.authenticateToken,
+  asyncHandler(async (req, res) => {
+    if (req.user.isRole(Role.Admin)) {
+      enableChaos = req.params.state === "true";
+    }
+
+    res.json({ chaos: enableChaos });
+  })
+);
+
+orderRouter.post("/", (req, res, next) => {
+  if (enableChaos && Math.random() < 0.5) {
+    throw new StatusCodeError("Chaos monkey", 500);
+  }
+  next();
+});
+
 module.exports = orderRouter;
